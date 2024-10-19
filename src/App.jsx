9fce3b47b3c846e15e01640670fbedb357fa5ce6
Nybobus <150648614+Nybobus@users.noreply.github.com>
2024-10-19 16:44:55 +0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './App.css'
+
+const App = () => {
+  return (
+    <>
+      <section>
+        <div className="container">
+          <div className="wrap">
+           <Card />
+           <Card2 />
+          </div>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/card2.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const card2 = () => {
+  return (
+    <>
+      <div className="card card2">
+              <img src="https://i.ibb.co/cNWqxGx/red.png" alt="" />
+              <h2>Nike x OFF-White</h2>
+              <p>Lorem ipsum dolor sit amet.</p>
+              <h3>$1599</h3>
+
+              <div>
+                <button>Buy now</button>
+                <button>⭐</button>
+              </div>
+            </div>
+    </>
+  )
+}
+
+export default card2
